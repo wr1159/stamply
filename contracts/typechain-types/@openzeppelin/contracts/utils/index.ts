67fc685bd2e70cd70944c0195efee45a1b986df4
wr1159 <chuweiron18@gmail.com>
2025-05-14 15:32:16 -0400

    Update typechain types

--- contracts/typechain-types/@openzeppelin/contracts/utils/index.ts
+++ contracts/typechain-types/@openzeppelin/contracts/utils/index.ts
@@ -3,4 +3,7 @@
 /* eslint-disable */
 import type * as introspection from "./introspection";
 export type { introspection };
+import type * as math from "./math";
+export type { math };
 export type { Pausable } from "./Pausable";
+export type { Strings } from "./Strings";

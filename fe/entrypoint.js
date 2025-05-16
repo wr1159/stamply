fda183accf6e37298fdfd3c367463c13b2e3c37e
Leow Kai Jie <leowkaijie@gmail.com>
2025-05-15 23:41:04 -0400

    fix: remove privy

--- fe/entrypoint.js
+++
@@ -1,6 +0,0 @@
-// Import required polyfills first
-import "fast-text-encoding";
-import "react-native-get-random-values";
-import "@ethersproject/shims";
-// Then import the expo router
-import "expo-router/entry";
